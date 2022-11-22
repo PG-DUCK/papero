11f9a7a922066d580ffa011284e1c43ac42f7f68
Mattia Barbanera <mattia.barbanera@cern.ch>
2022-11-22 16:53:50 +0100

    Update i2c_rx to ignore trigger when busy

--- logic/i2c_trig/trigger_rx.v
+++ logic/i2c_trig/trigger_rx.v
@@ -17,35 +17,36 @@
 module trigger_rx
 (
   //system clk & reset
-  input             clk,//system clock,10MHz-100MHz recommended
-  input             reset,//high effective
+  input 	    clk,//system clock,10MHz-100MHz recommended
+  input 	    reset,//high effective
+  input             iBusy,//high effective
 
   //system interface
-  input             busy_clear,//positive edge sensitive, must be enabled to clear "busy" to 0 when the sub-system is ready to accept a new trigger
-  output reg        trigger,//positive edge,indicating that the trigger arrives, and the DAQ could start to catch data
+  input 	    busy_clear,//positive edge sensitive, must be enabled to clear "busy" to 0 when the sub-system is ready to accept a new trigger
+  output reg 	    trigger,//positive edge,indicating that the trigger arrives, and the DAQ could start to catch data
   output reg [7:0]  sub_system_id,
   output reg [7:0]  trigger_type,
   output reg [31:0] trigger_serial,
-  output reg        crc_status, //high effective,indicating that the chesksum is OK now, and the "sub_system_id","trigger_type" and "trigger_serial" are all effective and could be loaded into the DAQ package
-  output reg        end_flag,
+  output reg 	    crc_status, //high effective,indicating that the chesksum is OK now, and the "sub_system_id","trigger_type" and "trigger_serial" are all effective and could be loaded into the DAQ package
+  output reg 	    end_flag,
 
   // sda transceiver interface
-  input             ro_sda,//
-  output wire       ren_sda,
-  output wire       de_sda,
-  output wire       di_sda,
+  input 	    ro_sda,//
+  output wire 	    ren_sda,
+  output wire 	    de_sda,
+  output wire 	    di_sda,
 
   // scl transceiver interface
-  input             ro_scl,//
-  output wire       ren_scl,
-  output wire       de_scl,
-  output wire       di_scl,
+  input 	    ro_scl,//
+  output wire 	    ren_scl,
+  output wire 	    de_scl,
+  output wire 	    di_scl,
 
   // busy transceiver interface
-  input             ro_busy,
-  output wire       ren_busy,
-  output wire       de_busy,
-  output wire       di_busy //high effective,indicating that the sub-system is busy with receiving trigger package and generating the DAQ data package
+  input 	    ro_busy,
+  output wire 	    ren_busy,
+  output wire 	    de_busy,
+  output wire 	    di_busy //high effective,indicating that the sub-system is busy with receiving trigger package and generating the DAQ data package
 );
 
 reg [3:0] state_i2c;
@@ -61,12 +62,15 @@ parameter [3:0] st_crc_step1            = 4'D8;
 parameter [3:0] st_crc_check            = 4'D9;
 
 wire scl,sda;
+reg start;
+reg sBusyFlag;
 reg busy;
-reg trigger_out,trigger_out_reg,trigger_out_reg2;
+reg trigger_out_reg,trigger_out_reg2;
 reg busy_clear_reg,busy_clear_reg2;
-reg stop;
+reg stop, stop_reg;
 reg scl_reg,scl_reg2;
 reg sda_reg,sda_reg2;
+reg sda_fedge;   
 reg [3:0] count_scl;
 reg [7:0] data;
 reg crc_reset,crc_en;
@@ -90,20 +94,32 @@ assign ren_busy = 1;        //enable receiver
 assign de_busy  = 1;        //disable driver
 assign di_busy  = busy;     //busy output
 
-always@(posedge clk) begin //start detection: from asynch to synch (MB)
-  if (reset==1) begin
-    trigger_out <= 0;
-  end else begin
-    if ((sda_reg2==1)&&(sda_reg==0)&&(scl==1)&&(stop==0)) begin
-      //sda falling edge
-      trigger_out <= 1;
-    end else if ((sda_reg2==1)&&(sda_reg==0)&&((scl==0)||(stop==1))) begin
-    //Comply with original Tianwei module; sda falling edge
-      trigger_out <= 0;
-    end
-  end
-end
 
+always@(posedge clk) begin //start detection
+   if (reset==1) begin
+      start <= 0;
+      sBusyFlag <= 0;
+   end else begin
+      if ((state_i2c==st_idle) && (sBusyFlag==0) && (iBusy == 0) && (sda_fedge==1)) begin
+	 //idle & not busy & sda falling edge
+	 start <= 1;
+      end else begin
+	 start <= 0;
+      end
+
+      //sBusyFlag: maintains high for the busy during the whole transaction
+      if ((state_i2c==st_idle) && (iBusy == 1) && (sda_fedge==1)) begin
+	 sBusyFlag = 1;
+      end
+      if (stop_reg==1) begin
+	 sBusyFlag = 0;	
+      end
+
+	
+  end
+end // always@ (posedge clk)
+
+   
 always@(posedge clk) begin //stop detection
   if (reset==1) begin
     stop <= 0;
@@ -123,17 +139,24 @@ always@(posedge clk) begin //sync register
     sda_reg<=1;
     scl_reg2<=1;
     sda_reg2<=1;
+    sda_fedge<=0;
     trigger_out_reg<=0;
     trigger_out_reg2<=0;
     trigger<=0;
+    end_flag<=0;
+    stop_reg<=0;
   end else begin
     scl_reg<=scl;
     scl_reg2<=scl_reg;
     sda_reg<=sda;
     sda_reg2<=sda_reg;
-    trigger_out_reg<=trigger_out;
+    sda_fedge<=sda_reg2 & ~(sda_reg);
+
+    trigger_out_reg<=start;
     trigger_out_reg2<=trigger_out_reg;
-    trigger<=trigger_out;
+    trigger<=start;
+    end_flag<=stop & ~sBusyFlag;
+    stop_reg<= stop;
   end
 end
 
@@ -193,7 +216,7 @@ begin
         end
       else
         begin
-          if((busy_clear_reg2==0)&&(busy_clear_reg==1))
+          if((stop==1))
             begin
               busy<=0;
             end
@@ -211,7 +234,6 @@ begin
       crc_reset<=1;
       crc_en<=0;
       crc_status<=0;
-      end_flag<=0;
       sub_system_id<=0;
       trigger_type<=0;
       trigger_serial<=0;
@@ -224,15 +246,11 @@ begin
             crc_reset<=1;
             crc_en<=0;
             crc_status<=0;
-            end_flag<=0;
-            if((trigger_out_reg==1)&&(trigger_out_reg2==0))
-              begin
+	    if (sda_fedge==1) begin
                 state_i2c<=st_sub_system_id;
-              end
-            else
-              begin
+            end else begin
                 state_i2c<=st_idle;
-              end
+            end
           end
       
         st_sub_system_id:
@@ -351,8 +369,12 @@ begin
         
         st_crc_check:
           begin
-            state_i2c<=st_idle;
-            end_flag<=1;
+            if (stop==1) begin
+	      state_i2c<=st_idle;
+	    end else begin
+	      state_i2c<=st_crc_check;
+	    end
+	     
             if(crc_recv[15:0]==crc_out[15:0])
               begin
                 crc_status<=1;
